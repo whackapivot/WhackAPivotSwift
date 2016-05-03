746829bbb4bea267113383b5582370aba8a311ab
Peter Alfvin <whackapivot@googlegroups.com>
2016-05-02 23:15:47 -0700

    Get travis working again by re-exporting secure environment variables

---
+++ set-environment-variables.sh
@@ -0,0 +1,4 @@
+travis encrypt GH_TOKEN=$GH_TOKEN --add env.global
+travis encrypt FASTLANE_PASSWORD=$FASTLANE_PASSWORD --add env.global
+travis encrypt KEY_PASSWORD=$KEY_PASSWORD --add env.global
+travis encrypt CERTIFICATE_PASSWORD=$CERTIFICATE_PASSWORD --add env.global 
