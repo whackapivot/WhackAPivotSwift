97da45711210bf1e30586298a59877b632f450a0
Peter Alfvin <whackapivot@googlegroups.com>
2016-05-03 00:23:18 -0700

    Yet another attempt to fix CI

--- set-environment-variables.sh
+++ set-environment-variables.sh
@@ -1,4 +1,3 @@
 travis encrypt GH_TOKEN=$GH_TOKEN --add env.global
 travis encrypt FASTLANE_PASSWORD=$FASTLANE_PASSWORD --add env.global
 travis encrypt KEY_PASSWORD=$KEY_PASSWORD --add env.global
-travis encrypt ENCRYPTION_PASSWORD=$ENCRYPTION_PASSWORD --add env.global 
